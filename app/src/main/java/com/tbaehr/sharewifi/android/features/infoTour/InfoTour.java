b8667e5a88e399cddc22d77975d6b1c43856f69c
Timo Bähr <timo.baehr@gmail.com>
2016-04-10 22:46:13 +0200

    Added new pages to info tour: * dummy login screen * Share options explanation

--- app/src/main/java/com/tbaehr/sharewifi/android/features/infoTour/InfoTour.java
+++ app/src/main/java/com/tbaehr/sharewifi/android/features/infoTour/InfoTour.java
@@ -40,10 +40,13 @@ public class InfoTour extends AppIntro2 {
         description = getString(R.string.infotour_slide3_text);
         addSlide(AppIntroFragment.newInstance(title, description, R.drawable.info_shared_by_me, android.R.color.transparent));
 
-        title = getString(R.string.infotour_slide4_title);
-        description = getString(R.string.infotour_slide4_text);
+        addSlide(new InfoShareWithFragment());
+
+        title = getString(R.string.infotour_slide5_title);
+        description = getString(R.string.infotour_slide5_text);
         addSlide(AppIntroFragment.newInstance(title, description, R.drawable.under_construction_400, android.R.color.transparent));
 
+        addSlide(new InfoLoginFragment());
         // OPTIONAL METHODS
         // Override bar/separator color.
         //setBarColor(Color.parseColor("#3F51B5"));
